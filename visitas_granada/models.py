b45699c5801059849d030807ecac4bb9c4d2532b
mati <mati331@correo.ugr.es>
2020-03-04 16:30:22 +0100

    add tareas 0 y 1

---
+++ visitas_granada/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Visita(models.Model):
+	nombre      = models.CharField(max_length=100)
+	descripción = models.CharField(max_length=1000)
+	likes       = models.IntegerField(default=0)
+
+class Comentario(models.Model):
+	visita      = models.ForeignKey(Visita, on_delete=models.CASCADE)
+	texto       = models.CharField(max_length=500)
