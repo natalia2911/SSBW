3ee1dff26b04062b985bb2c326b193ea3aaa27af
mati <mati331@correo.ugr.es>
2020-06-15 12:14:40 +0200

    termino tarea 3 closes #1

--- visitas_granada/urls.py
+++ visitas_granada/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path(r'^card/$', views.card, name='card'),
+    path('', views.index, name='card'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
--- visitas_granada/views.py
+++ visitas_granada/views.py
@@ -8,7 +8,7 @@ def index(request):
     visitas = Visita.objects.all()
     comentarios = Comentario.objects.all()
     context = {'visitas': visitas, 'comentarios': comentarios}
-    return render(request, 'base.html', context)
+    return render(request, 'card.html', context)
 
 def card(request):
     visitas = Visita.objects.all()
@@ -36,3 +36,22 @@ def añadir_visita(request):
 def editar_visita(request, id):
     visita = Visita.objects.get(pk=id)
     form = VisitaForm(instance=visita)
+
+def login():
+	if request.method == 'POST':
+		with open('data.txt', 'rb') as g:
+			aux = pickle.load(g)
+		pas= request.form['passwd']
+		user = request.form['user']
+		if request.form['passwd'] in aux:
+			session['user']=user
+			session['passwd']= pas
+			session['pages']=[]
+			return render_template('paginaPrincipal.html', user = session['user'], historial = session['pages'])
+		return render_template('paginaPrincipal.html')
+	else:
+		return render_template('login.html', user = False)
+
+def logout():
+	session.pop('user',None)
+	return redirect(url_for('index'))
