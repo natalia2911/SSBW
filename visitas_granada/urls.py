fc0ffa9cf72393882de33c1ebccb070c1be15fad
mati <mati331@correo.ugr.es>
2020-06-16 21:24:08 +0200

    añadir visita #2

--- visitas_granada/urls.py
+++ visitas_granada/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='card'),
     path('detalle/', views.detalle, name='detalle'),
+    path('añadir_visita/', views.añadir_visita, name='añadir_visita'),
 ]
--- visitas_granada/views.py
+++ visitas_granada/views.py
@@ -1,9 +1,10 @@
     #latest_visita_list = list(Visita.objects.order_by('likes')[:3])
     #template = loader.get_template('visitas_granada/index.html')
     #context = {'visita': latest_visita_list,'test': "test888"}
-from django.shortcuts import render
-from .models import Visita, Comentario
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Visita, Comentario, VisitaForm
 from django.contrib.auth import authenticate, login, logout
+
 
 
 def index(request):
@@ -19,21 +20,20 @@ def detalle(request):
     return render(request, 'detalle.html', context)
 
 def añadir_visita(request):
-    form = VisitaForm()
-			
+    			
     if request.method == 'POST':   # de vuelta con los datos
 
         form = VisitaForm(request.POST, request.FILES) #  bound the form
 
         if form.is_valid():
             form.save()
-            return redirect('index')
-			
-        context = {
-            'form': form
-        }
+            return index(request)
+        else:
+            print(form.errors)		
+    else:
+        form = VisitaForm()
     # GET o error	
-    return render(request, "añadir_visita.html", context)
+    return render(request, "añadir_visita.html", {'form': form})
 
 def editar_visita(request, id):
     visita = Visita.objects.get(pk=id)
